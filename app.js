78ab855221056c6a1a1e44db81a01f37238a2b29
BaeSangUn <zyxtkddns@naver.com>
2022-08-30 14:13:42 +0900

    DB 업데이트

--- app.js
+++ app.js
@@ -1,67 +1,53 @@
-import axios from "axios";
 import express from "express";
 import mysql from "mysql2/promise";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 4000;
 const pool = mysql.createPool({
   host: "localhost",
   user: "sbsst",
   password: "sbs123414",
-  database: "sellitems",
+  database: "team",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-});
-
-app.get("/Test1", async (req, res) => {
-  const { id, itemname, price } = req.params;
-  const [rows] = await pool.query(`SELECT * FROM items`, [id, itemname, price]);
-
-  res.json(rows);
-});
-
-app.get("/Login", async (req, res) => {
-  const [log] = await pool.query(`SELECT * FROM Logintable`);
-
-  res.json(log);
-});
-
-app.patch("/Item", async (req, res) => {
-  const { id, itemname, price } = req.params;
-
-  const [[item]] = await pool.query(
-    `
-    SELECT * FROM items
-    `,
-    [id, itemname, price]
-  );
-  res.json(item);
-});
-app.get("/Test1", async (req, res) => {
-  const [rows] = await pool.query(
-    `
-  SELECT *
-  FROM Logintable 
-  `
-  );
-
-  res.json(rows);
 });
 
 app.get("/test1", async (req, res) => {
   const [rows] = await pool.query(
     `
   SELECT *
-  FROM Logintable 
+  FROM user 
   `
   );
 
+  res.json(rows);
+});
+
+app.get("/test2", async (req, res) => {
+  const [rows] = await pool.query(
+    `
+    SELECT * FROM product
+  `
+  );
+
+  res.json(rows);
+});
+
+app.get("/KGDP", async (req, res) => {
+  const [rows] = await pool.query(
+    `
+    SELECT *
+    FROM product
+    WHERE category LIKE 'K%'
+    `
+  );
   res.json(rows);
 });
 
@@ -73,48 +59,18 @@ app.post("/test1/doLogin", async (req, res) => {
   const [[userRow]] = await pool.query(
     `
   SELECT *
-  FROM Logintable
+  FROM user
   WHERE id = ?
   AND
   pw = ?
   `,
     [id, pw]
   );
-  // console.log(userRow);
+  console.log(userRow);
   if (userRow) {
     return res.send(true);
   }
   res.send(false);
-});
-
-app.get("/test1/login", async (req, res) => {
-  console.log(req.body);
-  // const [rows] = await pool.query(
-  //   `
-  // SELECT *
-  // FROM user
-  // WHERE id = ?
-  // AND
-  // pw = ?
-  // `,
-  //   [id, pw]
-  // );
-  // // {
-  // //   rows ? res.json(true) : res.json(false);
-  // // }
-  // console.log(rows);
-  // if (rows.length === 0) {
-  //   res.json(false);
-  // } else if (rows.length !== 0) {
-  //   res.json(true);
-  // }
-  // res.json(rows);
-  // if (res.json) {
-  //   res.json(true);
-  // } else if (!res.json) {
-  //   res.json(false);
-  // }
-  res.send("login");
 });
 
 app.post("/test1", async (req, res) => {
@@ -127,7 +83,7 @@ app.post("/test1", async (req, res) => {
 
   const [row] = await pool.query(
     `
-    INSERT INTO Logintable
+    INSERT INTO user
     SET id = ?,
     pw = ?,
     name = ?
@@ -137,11 +93,32 @@ app.post("/test1", async (req, res) => {
 
   const [rows] = await pool.query(`
     SELECT *
-    FROM Logintable
+    FROM user
     `);
 
   res.json(rows);
 });
+
+app.post("/product", async (req, res) => {
+  const {
+    body: { prdId },
+  } = req;
+  console.log("prdId", prdId);
+
+  const [[prdRow]] = await pool.query(
+    `
+  SELECT *
+  FROM product
+  WHERE prdId = ?
+  `,
+    [prdId]
+  );
+  // console.log("prdRow", prdRow);
+
+  res.json(prdRow);
+  // res.send([prdRow]);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
